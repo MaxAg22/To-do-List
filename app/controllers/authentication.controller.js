a43ec980242aab6d5f64f359cd4620f6e98af056
MaxAguero22 <jeezmax17@gmail.com>
2025-07-18 18:38:49 -0300

    feat(authentication.controller): add user register logic with database

--- app/controllers/authentication.controller.js
+++ app/controllers/authentication.controller.js
@@ -14,60 +14,76 @@ export const usuarios = [{
 
 async function login(req,res){
 
-  console.log("input: " + JSON.stringify(req.body));
+  // Check input
+  console.log("Login input: ", req.body);
   const user = req.body.user;
   const password = req.body.password;
   if(!user || !password){
     return res.status(400).send({status:"Error",message:"Los campos están incompletos"})
   }
 
-  // database query
+  // Check database
   const connection = await getConnection();
-  const usuariosDB = await connection.query("SELECT * from users");
+  const userDB = await connection.query("SELECT * from user WHERE user = ?", user);
+  if(userDB.length === 0) return res.status(400).send({status:"Error",message:"Error durante login"});
 
-  // check if user is in database
-  const usuarioAResvisar = usuariosDB.find(usuario => usuario.user == user);
-  if(!usuarioAResvisar){
-    return res.status(400).send({status:"Error",message:"Error durante login"})
-  }
-
-  const loginCorrecto = await bcryptjs.compare(password, usuarioAResvisar.password);
+  // Compare password
+  const loginCorrecto = await bcryptjs.compare(password, userDB[0].password);
   if(!loginCorrecto){
     return res.status(400).send({status:"Error",message:"Error durante login"})
   }
 
+  // Create new token
   const token = jsonwebtoken.sign(
-    {user:usuarioAResvisar.user},
+    {
+      user:userDB[0].user,
+      email: userDB[0].email
+    },
     process.env.JWT_SECRET,
     {expiresIn:process.env.JWT_EXPIRATION});
 
-    const cookieOption = {
-      expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
-      path: "/"
-    }
-    res.cookie("jwt",token,cookieOption);
-    res.send({status:"ok",message:"Usuario loggeado",redirect:"/admin"});
+  const cookieOption = {
+    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
+    path: "/"
+  }
+
+  res.cookie("jwt",token,cookieOption);
+  res.send({status:"ok",message:"Usuario loggeado",redirect:"/admin"});
+
+  console.log("Usuario encontrado con éxito: ", userDB[0]);
 }
 
 async function register(req,res){
+
+  // Check input
+  console.log("Register input: ", req.body);
   const user = req.body.user;
   const password = req.body.password;
   const email = req.body.email;
   if(!user || !password || !email){
     return res.status(400).send({status:"Error",message:"Los campos están incompletos"})
   }
-  const usuarioAResvisar = usuarios.find(usuario => usuario.user === user);
-  if(usuarioAResvisar){
-    return res.status(400).send({status:"Error",message:"Este usuario ya existe"})
+
+  // Check database
+  const connection = await getConnection();
+  const userDB = await connection.query("SELECT * from user WHERE user = ?", user);
+
+  if(userDB.length > 0) {
+    console.log("Este usaurio ya existe");
+    return res.status(400).send({status:"Error",message:"Este usuario ya existe"});
   }
+
+  // Instert
   const salt = await bcryptjs.genSalt(5);
   const hashPassword = await bcryptjs.hash(password,salt);
-  const nuevoUsuario ={
-    user, email, password: hashPassword
-  }
-  usuarios.push(nuevoUsuario);
-  console.log(usuarios);
-  return res.status(201).send({status:"ok",message:`Usuario ${nuevoUsuario.user} agregado`,redirect:"/"})
+
+  await connection.query(
+    "INSERT INTO user (user, email, password) VALUES (?, ?, ?)",
+    [user, email, hashPassword]
+  );
+
+  console.log("Usuario registrado con éxito: ", {user, email, hashPassword});
+  return res.status(201).send({status:"ok",message:`Usuario ${user} agregado`,redirect:"/"})
 }
 
 export const methods = {
--- app/middlewares/authorization.js
+++ app/middlewares/authorization.js
@@ -1,31 +1,41 @@
 import jsonwebtoken from "jsonwebtoken";
 import dotenv from "dotenv";
 import {usuarios} from "./../controllers/authentication.controller.js";
+import { getConnection } from '../database.js';
+
 
 dotenv.config();
 
-function soloAdmin(req,res,next){
-  const logueado = revisarCookie(req);
+async function soloAdmin(req,res,next){
+  const logueado = await revisarCookie(req);
   if(logueado) return next();
   return res.redirect("/")
 }
 
-function soloPublico(req,res,next){
-  const logueado = revisarCookie(req);
+async function soloPublico(req,res,next){
+  const logueado = await revisarCookie(req);
   if(!logueado) return next();
   return res.redirect("/admin")
 }
 
-function revisarCookie(req){
+async function revisarCookie(req){
   try{
     const cookieJWT = req.headers.cookie.split("; ").find(cookie => cookie.startsWith("jwt=")).slice(4);
     const decodificada = jsonwebtoken.verify(cookieJWT,process.env.JWT_SECRET);
-    console.log(decodificada)
-    const usuarioAResvisar = usuarios.find(usuario => usuario.user === decodificada.user);
-    console.log(usuarioAResvisar)
-    if(!usuarioAResvisar){
-      return false
-    }
+    console.log("Cookie decodificada: ", JSON.stringify(decodificada));
+    
+    //const usuarioAResvisar = usuarios.find(usuario => usuario.user === decodificada.user);
+    //console.log(usuarioAResvisar)
+    //if(!usuarioAResvisar){
+    //  return false
+    //}
+    //return true;
+
+    const connection = await getConnection();
+    const userDB = await connection.query("SELECT * from user WHERE user = ?", decodificada.user);
+    console.log("Usuario encontrado en el cookie check: ", userDB[0]);
+    if(userDB.length === 0) return false;
+    req.user = userDB[0];
     return true;
   }
   catch{
@@ -38,3 +48,4 @@ export const methods = {
   soloAdmin,
   soloPublico,
 }
+
